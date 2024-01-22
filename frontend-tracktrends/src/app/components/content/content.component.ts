c28d099993aa6c4df4fb48f3247a3a6e1834ea54
jacknormand <jack.normand15@gmail.com>
2024-01-22 13:23:08 -0800

    Home page created
    home page created and credentials for database stored in environment variables.

--- frontend-tracktrends/src/app/components/content/content.component.ts
+++ frontend-tracktrends/src/app/components/content/content.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AverageComponent } from './visuals/average/average.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [AverageComponent],
+  imports: [CommonModule, AverageComponent, HttpClientModule],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
+
+  httpClient = inject(HttpClient);
+  chartData: number[][] = [];
+
+  ngOnInit(): void {
+    this.makeAPICall()
+  }
+
+  makeAPICall(): void {
+    const apiUrl = `http://localhost:8080/api/top100count`;
+
+  this.httpClient.get(apiUrl).subscribe(
+    (data: any) => {
+       // Process the API response data
+       // show charts if valid response. uses data service
+       this.chartData = data;
+    },
+    (error) => {
+       console.error('API Error:', error);
+    }
+ );
+    
+
+  }
+
+
 
 }
